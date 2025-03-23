cb44f9c6f69eddf87c7c0c356ec43e8039748aa3
TNHo <ttnho08@gmail.com>
2025-03-23 12:37:40 -0400

    Create Bullets.hx

---
+++ source/Bullets.hx
@@ -0,0 +1,16 @@
+/*
+ * Something about touhou bullets in the overworld
+ */
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.input.keyboard.FlxKey;
+import flixel.math.FlxPoint;
+import flixel.util.FlxSpriteUtil;
+
+class Bullets extends FlxSprite 
+{
+    public function new(x:Float = 0, y:Float = 0) 
+    {
+        super(x, y);
+    }
+}
